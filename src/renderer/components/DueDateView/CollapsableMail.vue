501fadfa22104c144309a717a89fa2829b318657
Nico Verrijdt <nvrrdt@gmail.com>
2018-06-17 20:52:07 +0200

    Complete json and rework its presentation

--- src/renderer/components/DueDateView/CollapsableMail.vue
+++ src/renderer/components/DueDateView/CollapsableMail.vue
@@ -2,7 +2,7 @@
   <div role="tablist">
     <b-card no-body class="mb-1" v-for="(mail, index) in mails" :key="index">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-btn block v-b-toggle="'index-'+index" variant="link" v-on:click="getMailBody(index)">{{ mail.mail }}</b-btn>
+        <b-btn block v-b-toggle="'index-'+index" variant="link" v-on:click="getMailBody(index)">{{ mail.title }}</b-btn>
       </b-card-header>
       <b-collapse :id="'index-'+index" accordion="my-accordion" role="tabpanel">
         <b-card-body>
@@ -26,7 +26,7 @@
     },
     methods: {
       getMailBody (index) {
-        var data = this.mails[index].mail
+        var data = this.mails[index].body
         this.$root.$emit('emailBody', data)
       }
     }
