cf4bde2e9115cbdf1ae147965783022e082b6fa0
nvrrdt <nicoverrijdt@gmail.com>
2018-07-12 19:55:30 +0200

    Improvement for column width in collapse header

--- src/renderer/components/DueDateView/CollapsableMail.vue
+++ src/renderer/components/DueDateView/CollapsableMail.vue
@@ -5,20 +5,20 @@
         <b-btn block class="text-left p-0 ml-2" v-b-toggle="'index-'+index" variant="link" v-on:click="getMailBody(index)">
           <div class="container">
             <div class="row">
-              <div class="col-3">
+              <div class="col-3 col-md-4 col-lg-4">
                 {{ project.project }}
               </div>
-              <div class="col-3">
+              <div class="col-3 col-md-3 col-lg-3">
                 <span class="border border-dark">
                   <b-badge pill v-for="cat1 in getCategories1(index)" :key="cat1" v-bind:style="{ background: getColors1(cat1) }">&nbsp;</b-badge>
                 </span>
               </div>
-              <div class="col-3">
+              <div class="col-3 col-md-3 col-lg-3">
                 <span class="border border-dark">
                   <b-badge pill v-for="cat2 in getCategories2(index)" :key="cat2" v-bind:style="{ background: getColors2(cat2) }">&nbsp;</b-badge>
                 </span>
               </div>
-              <div class="col-3">
+              <div class="col-3 col-md-2 col-lg-2">
                 {{ project.duedate }}
               </div>
             </div>
